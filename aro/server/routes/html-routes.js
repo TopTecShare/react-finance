0ee2ef75202616581e7236416e6a1d8ef637a49c
TopTecShare <eestimorol.ice9208g@gmail.com>
2021-02-23 12:45:16 -0500

    Added heroku postbuild scripts

--- aro/server/routes/html-routes.js
+++ aro/server/routes/html-routes.js
@@ -24,8 +24,8 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/mainpage", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "/signup.html"));
   });
 
   app.get("/signup", (req, res) => {
